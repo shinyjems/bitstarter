5a5826ab202c06997eebafcceb63ad2c83d3fa70
James Kim <kiomanery@gmail.com>
2013-07-11 05:45:33 +0000

    trying to fix again 9

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(20);
-buffer.write(fs.readFileSync('index.html');
+var buff = new Buffer(fs.readFileSync('index.html'));
+//buff.write(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
