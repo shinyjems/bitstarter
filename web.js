51596254dc8c2df52ce57e371ce66662ecd8bae5
James Kim <kiomanery@gmail.com>
2013-07-11 05:38:16 +0000

    trying to fix again 7

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-//var buffer = new Buffer(20);
-//buffer.write(fs.readFileSync('index.html');
+var buffer = new Buffer(20);
+buffer.write(fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
