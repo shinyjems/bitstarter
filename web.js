d8d2d4bac12fff3787866a547762571e3ef6c524
James Kim <kiomanery@gmail.com>
2013-07-11 05:41:53 +0000

    trying to fix again 8

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer = new Buffer(20);
 buffer.write(fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
